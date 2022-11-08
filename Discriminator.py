e3e3ce374f9bd90fd9de36f636091c6ffe0452d1
qkhanh711 <91941803+qkhanh711@users.noreply.github.com>
2022-11-08 18:17:35 +0700

    add file.py

---
+++ Discriminator.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn 
+from torchvision import transforms
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+class Discriminaor(nn.Module):
