2deb7bf910421efbebf7ae1317a1f7056cd43c27
Mink <88665124+Minhdd1413@users.noreply.github.com>
2022-11-09 22:39:49 +0700

    update Cus data

--- CustomDataset.py
+++ CustomDataset.py
@@ -1,22 +1,19 @@
-# Import
-import os
-import glob # lấy danh sách vào tên thư viện theo điều kiện
-import random
+import glob # đọc đuôi file 
 import PIL.Image as Img
 from torch.utils.data import Dataset
 from torchvision import transforms
-
+import torch
 class Face_Data(Dataset): # Dataset template 
     def __init__(self, root, transform = transforms):
         self.transform = transform
 
-        # Lấy danh sách tên file và thư mục theo điều kiện: glob.glob(pattern, *, recursive=False)
+        # Sắp sếp + đọc filename
         self.real = sorted(glob.glob(root + "/*"))
-
+        self.fake = torch.tensor(3, 512, 512)
     def __getitem__(self, index):
         item_real = self.transform(Img.open(self.real[index % len(self.real)]))
-
-        return { "1" : item_real}
+        item_fake = self.fake
+        return { "1" : item_real, "0" : item_fake}
 
     def __len__(self):
         return len(self.real)
@@ -25,3 +22,4 @@ transforms = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
+
