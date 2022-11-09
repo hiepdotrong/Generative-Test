3903939a110467351ed30ea19d3e0eaa672784d6
qkhanh711 <91941803+qkhanh711@users.noreply.github.com>
2022-11-09 20:39:34 +0700

    update Code main utils config

--- Config.py
+++ Config.py
@@ -9,12 +9,18 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 SAVE_MODEL = True
 LOAD_MODEL = False
 LEARNING_RATE = 1e-3
-BATCH_SIZES = [32, 32, 32, 16, 16, 16, 16, 8, 4]
+BATCH_SIZE = 64
 CHANNELS_IMG = 3
 Z_DIM = 256  # 512 in paper
 IN_CHANNELS = 256  # 512 in paper
 CRITIC_ITERATIONS = 1
 LAMBDA_GP = 10
-PROGRESSIVE_EPOCHS = [32] * len(BATCH_SIZES)
+NUM_EPOCHS = 5
 FIXED_NOISE = torch.randn(8, Z_DIM, 1, 1).to(DEVICE)
 NUM_WORKERS = 4
+
+# Hyperparameters etc
+IMAGE_SIZE = 64
+FEATURES_CRITIC = 64
+FEATURES_GEN = 64
+WEIGHT_CLIP = 0.01
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-
+import torchvision
 from Generator import Generator
 from Discriminator import Discriminator 
 
@@ -17,99 +17,96 @@ from utils import (
     gradient_penalty,
     save_checkpoint,
     load_checkpoint,
+    initialize_weights,
+)
+from torch.utils.tensorboard import SummaryWriter
+
+
+
+transforms = transforms.Compose(
+    [
+        transforms.Resize(config.IMAGE_SIZE),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            [0.5 for _ in range(config.CHANNELS_IMG)], [0.5 for _ in range(config.CHANNELS_IMG)]
+        ),
+    ]
 )
 
+dataset = datasets.MNIST(root="dataset/", transform=transforms, download=True)
+#comment mnist and uncomment below if you want to train on CelebA dataset
+#dataset = datasets.ImageFolder(root="celeb_dataset", transform=transforms)
+loader = DataLoader(dataset, batch_size=config.BATCH_SIZE, shuffle=True)
 
+# initialize gen and disc/critic
+gen = Generator(config.Z_DIM, config.CHANNELS_IMG, config.FEATURES_GEN).to(config.DEVICE)
+critic = Discriminator(config.CHANNELS_IMG, config.FEATURES_CRITIC).to(config.DEVICE)
+initialize_weights(gen)
+initialize_weights(critic)
 
-torch.backends.cudnn.benchmarks = True
+# initializate optimizer
+opt_gen = optim.RMSprop(gen.parameters(), lr=config.LEARNING_RATE)
+opt_critic = optim.RMSprop(critic.parameters(), lr=config.LEARNING_RATE)
 
-def train_fn(critic,gen,loader,dataset,step,alpha,opt_critic,opt_gen,tensorboard_step,writer,scaler_gen,scaler_critic,):
-    loop = tqdm(loader, leave=True)
-    for batch_idx, (real, _) in enumerate(loop):
-        real = real.to(config.DEVICE)
-        cur_batch_size = real.shape[0]
+# for tensorboard plotting
+fixed_noise = torch.randn(32, config.Z_DIM, 1, 1).to(config.DEVICE)
+writer_real = SummaryWriter(f"logs/real")
+writer_fake = SummaryWriter(f"logs/fake")
+step = 0
 
-        # Train Critic: max E[critic(real)] - E[critic(fake)] <-> min -E[critic(real)] + E[critic(fake)]
-        # which is equivalent to minimizing the negative of the expression
-        noise = torch.randn(cur_batch_size, config.Z_DIM, 1, 1).to(config.DEVICE)
+gen.train()
+critic.train()
 
-        with torch.cuda.amp.autocast():
-            fake = gen(noise, alpha, step)
-            critic_real = critic(real, alpha, step)
-            critic_fake = critic(fake.detach(), alpha, step)
-            gp = gradient_penalty(critic, real, fake, alpha, step, device=config.DEVICE)
-            loss_critic = (
-                -(torch.mean(critic_real) - torch.mean(critic_fake))
-                + config.LAMBDA_GP * gp
-                + (0.001 * torch.mean(critic_real ** 2))
-            )
+for epoch in range(config.NUM_EPOCHS):
+    # Target labels not needed! <3 unsupervised
+    for batch_idx, (data, _) in enumerate(loader):
+        data = data.to(config.DEVICE)
+        cur_batch_size = data.shape[0]
 
-        opt_critic.zero_grad()
-        scaler_critic.scale(loss_critic).backward()
-        scaler_critic.step(opt_critic)
-        scaler_critic.update()
+        # Train Critic: max E[critic(real)] - E[critic(fake)]
+        for _ in range(config.CRITIC_ITERATIONS):
+            noise = torch.randn(cur_batch_size, config.Z_DIM, 1, 1).to(config.DEVICE)
+            fake = gen(noise)
+            critic_real = critic(data).reshape(-1)
+            critic_fake = critic(fake).reshape(-1)
+            loss_critic = -(torch.mean(critic_real) - torch.mean(critic_fake))
+            critic.zero_grad()
+            loss_critic.backward(retain_graph=True)
+            opt_critic.step()
+
+            # clip critic weights between -0.01, 0.01
+            for p in critic.parameters():
+                p.data.clamp_(-config.WEIGHT_CLIP, config.WEIGHT_CLIP)
 
         # Train Generator: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
-        with torch.cuda.amp.autocast():
-            gen_fake = critic(fake, alpha, step)
-            loss_gen = -torch.mean(gen_fake)
+        gen_fake = critic(fake).reshape(-1)
+        loss_gen = -torch.mean(gen_fake)
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
 
-        opt_gen.zero_grad()
-        scaler_gen.scale(loss_gen).backward()
-        scaler_gen.step(opt_gen)
-        scaler_gen.update()
-
-        # Update alpha and ensure less than 1
-        alpha += cur_batch_size / ((config.PROGRESSIVE_EPOCHS[step] * 0.5) * len(dataset))
-        alpha = min(alpha, 1)
-
-        if batch_idx % 500 == 0:
-            with torch.no_grad():
-                fixed_fakes = gen(config.FIXED_NOISE, alpha, step) * 0.5 + 0.5
-
-        loop.set_postfix(gp=gp.item(),loss_critic=loss_critic.item(),)
-
-    return tensorboard_step, alpha
-
-
-def main():
-    # initialize gen and disc
-    gen = Generator(config.Z_DIM, config.IN_CHANNELS, img_channels=config.CHANNELS_IMG).to(config.DEVICE)
-    critic = Discriminator(config.Z_DIM, config.IN_CHANNELS, img_channels=config.CHANNELS_IMG).to(config.DEVICE)
-
-    # initialize optimizers and scalers for FP16 training
-    opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE, betas=(0.0, 0.99))
-    opt_critic = optim.Adam(critic.parameters(), lr=config.LEARNING_RATE, betas=(0.0, 0.99)
-                            )
-    scaler_critic = torch.cuda.amp.GradScaler()
-    scaler_gen = torch.cuda.amp.GradScaler()
-
-    if config.LOAD_MODEL:
-        load_checkpoint(config.CHECKPOINT_GEN, gen, opt_gen, config.LEARNING_RATE,)
-        load_checkpoint(config.CHECKPOINT_CRITIC, critic, opt_critic, config.LEARNING_RATE,)
-
-    gen.train()
-    critic.train()
-
-    tensorboard_step = 0
-    # start at step that corresponds to img size that we set in config
-    step = int(log2(config.START_TRAIN_AT_IMG_SIZE / 4))
-    for num_epochs in config.PROGRESSIVE_EPOCHS[step:]:
-        alpha = 1e-5  # start with very low alpha
-        loader, dataset = get_loader(4 * 2 ** step)  # 4->0, 8->1, 16->2, 32->3, 64 -> 4
-        print(f"Current image size: {4 * 2 ** step}")
-
-        for epoch in range(num_epochs):
-            print(f"Epoch [{epoch+1}/{num_epochs}]")
-            tensorboard_step, alpha = train_fn(critic,gen,loader, dataset,step,alpha,opt_critic,opt_gen,tensorboard_step,writer,scaler_gen,scaler_critic,
+        # Print losses occasionally and print to tensorboard
+        if batch_idx % 100 == 0 and batch_idx > 0:
+            gen.eval()
+            critic.eval()
+            print(
+                f"Epoch [{epoch}/{config.NUM_EPOCHS}] Batch {batch_idx}/{len(loader)} \
+                  Loss D: {loss_critic:.4f}, loss G: {loss_gen:.4f}"
             )
 
-            if config.SAVE_MODEL:
-                save_checkpoint(gen, opt_gen, filename=config.CHECKPOINT_GEN)
-                save_checkpoint(critic, opt_critic, filename=config.CHECKPOINT_CRITIC)
+            with torch.no_grad():
+                fake = gen(noise)
+                # take out (up to) 32 examples
+                img_grid_real = torchvision.utils.make_grid(
+                    data[:32], normalize=True
+                )
+                img_grid_fake = torchvision.utils.make_grid(
+                    fake[:32], normalize=True
+                )
 
-        step += 1  # progress to the next img size
+                writer_real.add_image("Real", img_grid_real, global_step=step)
+                writer_fake.add_image("Fake", img_grid_fake, global_step=step)
 
-
-if __name__ == "__main__":
-    main()
+            step += 1
+            gen.train()
+            critic.train()
