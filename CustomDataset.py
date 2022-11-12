2a551b7696056337ad3af611f0c088bed465ce14
Trọng Hiệp Đỗ <hiepdotrong23@gmail.com>
2022-11-12 23:01:51 +0700

    Hiep

--- CustomDataset.py
+++ CustomDataset.py
@@ -1,23 +1,22 @@
 import os
 from torch.utils.data import Dataset
-from skimage import io
 import pandas as pd
 from PIL import Image
 
 class Custom(Dataset):
-    def __init__(self, csv_file, root, transforms = None):
+    def __init__(self, csv_file, file_data, transforms = None):
         self.csv = pd.read_csv(csv_file)
-        self.root = root
+        self.file_data = file_data
         self.transform = transforms
         
     def __len__(self):
         return len(self.csv)
     
     def __getitem__(self, index):
-        img_path = os.path.join(self.root, self.csv.iloc[index, 0])
+        img_path = os.path.join(self.file_data, self.csv.iloc[index, 0])
         read = Image.open(img_path)
                 
         if self.transform:
             image = self.transform(read)
-            return image
+        return image
 
